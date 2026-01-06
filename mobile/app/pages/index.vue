7a537765830579f34ead238aaf4cbe235dfb8a05
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-01-06 14:38:56 +0100

    update

--- mobile/app/pages/index.vue
+++ mobile/app/pages/index.vue
@@ -1,5 +1,1 @@
-<template>
-  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse laboriosam
-  magnam dolorem deleniti hic ad soluta? Minus, eius. Voluptates dolores sint in
-  perferendis obcaecati, illum sed similique. Eius, ratione obcaecati.
-</template>
+<template>hello acho</template>
