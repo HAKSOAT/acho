21c3f9ef4806d5820cda51a1d4d7f82295324941
Emmanuel <freelanceokeks@gmail.com>
2026-01-17 18:23:28 +0000

    implement u.i in flutter

--- mobile_app/lib/home.dart
+++ mobile_app/lib/home.dart
@@ -1,11 +1,88 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeApp extends StatelessWidget {
   const HomeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const SizedBox.shrink();
+    double width = MediaQuery.sizeOf(context).width;
+
+    //TODO: cap at 5
+    late List<String> searchedItems = ['A', 'B', 'C'];
+    late List<String> recentDocuments = [
+      'A',
+      'B',
+      'C'
+    ]; //TODO:switch to class, so we can access more attributes outside of title
+
+    return SizedBox.shrink(
+        child: Column(
+      children: [
+        SizedBox(
+          height: 70,
+          width: width - 40,
+          child: SearchBar(
+            leading: const Icon(Icons.search),
+            hintText: "Search...",
+
+            shape: WidgetStateProperty.all(
+              const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                    Radius.circular(3)), // This makes the corners square
+              ),
+            ),
+
+            backgroundColor: WidgetStateProperty.all(Colors.grey[200]),
+            elevation: WidgetStateProperty.all(0), // Flat style
+
+            onChanged: (text) {
+              //TODO: Handle search logic here
+              //spawn process into a new isolate to prevent u.i jank
+            },
+            onSubmitted: (text) {
+              //TODO: Handle enter key press,
+              //TODO: similar to above depending on latency we may just use this
+            },
+          ),
+        ),
+        const ListTile(
+            leading: Text(
+          "Recent Searches",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        )),
+        Row(
+            children: List.generate(searchedItems.length, (int index) {
+          return SizedBox(
+              width: 70,
+              child: TextButton(
+                //TODO: style to make borders visible
+                onPressed: () {
+                  //TODO: Handle click, popular search bar with text controller
+                },
+                child:
+                    Text(searchedItems[index]), // Display results from search
+              ));
+        })),
+        const ListTile(
+            leading: Text(
+          "Recent Documents",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        )),
+        Column(
+            children: List.generate(recentDocuments.length, (int index) {
+          return ListTile(
+            leading: const Icon(Icons.picture_as_pdf),
+            onTap: () {
+              // Handle click
+            },
+            trailing: const Icon(Icons.chevron_right),
+            title: Text(recentDocuments[index]), // Display results from search
+          );
+        }))
+      ],
+    ));
   }
 }
--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/src/rust/api/simple.dart';
 import 'package:mobile_app/src/rust/frb_generated.dart';
+import 'package:mobile_app/settings.dart';
+import 'package:mobile_app/home.dart';
 
 Future<void> main() async {
   await RustLib.init();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  PageController pageController = PageController();
+
+  int selectIndex = 0;
+  void onPageChanged(int index) {
+    setState(() {
+      selectIndex = index;
+    });
+  }
+
+  void onItemTap(int selectedItems) {
+    pageController.jumpToPage(selectedItems);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
-        ),
-      ),
+          appBar: AppBar(title: const Text('Acho')),
+          body: PageView(
+            children: [HomeApp(), SettingsApp()],
+            controller: pageController,
+            onPageChanged: onPageChanged,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              onTap: onItemTap,
+              selectedItemColor: Colors.brown,
+              items: const [
+                BottomNavigationBarItem(
+                  backgroundColor: Colors.red,
+                  label: 'Home',
+                  icon: Icon(Icons.home_filled),
+                  activeIcon: HomeApp(),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Settings',
+                  icon: Icon(Icons.settings),
+                  activeIcon: SettingsApp(),
+                )
+              ])),
     );
   }
 }
