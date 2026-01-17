f9c480da3bdb7c14dc9f93a5f43785ab3dddf3b1
Emmanuel <freelanceokeks@gmail.com>
2026-01-17 16:02:13 +0000

    Settings Page for Acho

---
+++ mobile_app/lib/folder.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FolderApp extends StatelessWidget {
+  const FolderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const SizedBox.shrink();
+  }
+}
---
+++ mobile_app/lib/settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingsApp extends StatelessWidget {
+  const SettingsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const SizedBox.shrink();
+  }
+}
