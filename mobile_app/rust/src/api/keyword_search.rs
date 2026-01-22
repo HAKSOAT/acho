89422cb047a9c0844ca2df55b0b9ce9dd1dd13e1
Emmanuel <freelanceokeks@gmail.com>
2026-01-22 20:00:33 +0000

    attempt at connecting flutter_rust_bridge

--- mobile_app/rust/src/api/keyword_search.rs
+++ mobile_app/rust/src/api/keyword_search.rs
@@ -1,20 +1,20 @@
-use seekstorm::index::{Index, open_index};
+use seekstorm::index::{ open_index};
+use seekstorm::index::IndexArc;
 use std::path::Path;
-use std::sync::Arc;
-use tokio::sync::RwLock;
 
-struct SearchFn {
-    path_to_index: String,
+
+pub struct SearchFn {
+    pub path_to_index: String,
 }
 
 impl SearchFn {
     pub async fn new(&self) -> SearchFn {
         use seekstorm::index::{
             AccessType, FrequentwordType, IndexMetaObject, NgramSet, SimilarityType, StemmerType,
-            StopwordType, TokenizerType, create_index,
+            StopwordType, TokenizerType, FieldType, create_index, SchemaField
         };
 
-        let schema = SchemaField::new("title".to_string(), true, true, FieldType::String16, false, false, 1.0, false, false);
+        let schema = vec![SchemaField::new("title".to_string(), true, true, FieldType::String16, false, false, 1.0, false, false)];
         let meta = IndexMetaObject {
             id: 0,
             name: "acho_index".into(),
@@ -28,8 +28,8 @@ impl SearchFn {
             spelling_correction: None,
             query_completion: None,
         };
-        let segment_number_bits1 = 11;
-        let index_arc = create_index(
+        let _segment_number_bits1 = 11;
+         create_index(
             Path::new(self.path_to_index.as_str()),
             meta,
             &schema,
@@ -45,20 +45,19 @@ impl SearchFn {
         }
     }
 
-    pub async fn ingest_pdf_dir(&self, dir_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn ingest_pdf_dir(&self, dir_path: &Path) {
         use seekstorm::ingest::IndexPdfFile;
 
         let file_path = Path::new(&dir_path);
-        let index_arc = open_index(Path::new(self.path_to_index.as_str()), false).await;
+         let index_arc: Result<IndexArc, String> = open_index(Path::new(self.path_to_index.as_str()), false).await;
 
         match index_arc {
-            Ok(mut index_arc) => {
+            Ok(index_arc) => {
                 let _ = index_arc.index_pdf_file(file_path).await;
             }
 
-            Err(error) => {
-                // Err(error.into());
-                println!("error: {}", error)
+            Err(_error) => {
+                 Err(_error.into());
             }
         }
 
@@ -66,16 +65,17 @@ impl SearchFn {
     }
 
     ///For reset functionality, clearing index
-    pub async fn delete_index(&self) -> () {
-        let index_arc = open_index(Path::new(self.path_to_index.as_str()), false).await;
+    pub async fn delete_index(&self) {
+        let index_arc: Result<IndexArc, String> = open_index(Path::new(self.path_to_index.as_str()), false).await;
 
         match index_arc {
             Ok(index_arc) => {
-                let _ = index_arc.write().await.delete_index();
+               index_arc.write().await.delete_index();
             }
 
-            Err(error) => {
+            Err(_) => {
                 // Err(error.into());
             }
         }
     }
+}
